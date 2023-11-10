70721dd9431e0fd314adce021bad6b690fac9cf0
BhargavUPPU <119610134+BhargavUPPU@users.noreply.github.com>
2023-11-10 23:44:45 +0530

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,9 @@
+const start=document.getElementById("start");
+const stop=document.getElementById("stop");
+const reset=document.getElementById("reset");
+const stopWatch=document.getElementById("stopWatch");
+let time=time/100;
+function watchStart(){
+    const minutes= Math.floor(time*60);
+    const seconds=
+}
